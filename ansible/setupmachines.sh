6d39ceb8a30a36f999e0f88496fbe8472008c580
GF09 <20781704+gf09@users.noreply.github.com>
2023-02-20 10:44:23 -0700

    Update

--- ansible/setupmachines.sh
+++ ansible/setupmachines.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
  docker ps
- docker run --name target1 -d ubuntu-ssh 
+ docker run --name target1 -d copperpointtraining 
  #mmumshad/ubuntu-ssh-enabled
  ansible target1 -m ping -i ~/ansible/inventory.txt
- docker run --name target2 -d ubuntu-ssh
+ docker run --name target2 -d copperpointtraining
  ansible target2 -m ping -i ~/ansible/inventory.txt
- docker run --name target3 -d ubuntu-ssh
+ docker run --name target3 -d copperpointtraining
  ansible target3 -m ping -i ~/ansible/inventory.txt
- docker run --name db_and_web_server1 -d ubuntu-ssh
+ docker run --name db_and_web_server1 -d copperpointtraining
  ansible db_and_web_server1 -m ping -i ~/ansible/inventory.txt
- docker run --name db_and_web_server2 -d ubuntu-ssh
+ docker run --name db_and_web_server2 -d copperpointtraining
  ansible db_and_web_server2 -m ping -i ~/ansible/inventory.txt
  docker ps
 df -h
